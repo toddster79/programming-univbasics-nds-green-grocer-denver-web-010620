41b9a392c993b802d5533f484bc55fa1d1fe2b4c
Todd Carlson <todd.carlson3030@me.com>
2019-12-27 19:07:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -58,9 +58,11 @@ cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  counter = 0 
+  while counter < cart.length do
+    
+    counter += 1 
+  end 
 end
 
 def checkout(cart, coupons)
